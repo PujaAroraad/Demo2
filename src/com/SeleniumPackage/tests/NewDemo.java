a5c3757af0729e6bb1bd41818fcf02fe7a8ff1f3
pujaarora <pujaarora@pujaarora.Nagarro.local>
2021-06-28 02:29:09 +0530

    1st commit

---
+++ src/com/SeleniumPackage/tests/NewDemo.java
@@ -0,0 +1,11 @@
+package com.SeleniumPackage.tests;
+
+public class NewDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Demo");
+	}
+
+}
